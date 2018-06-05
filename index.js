0d7a6eb54bfd5bdf3bee8ff09a8f6818077a8c63
Naveen <naveenrk93@gmail.com>
2018-06-05 14:14:36 -0700

    Finishing touches, Almost done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const router = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/ApiServ');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(PORT);
+console.log('Server listening on :', PORT);
